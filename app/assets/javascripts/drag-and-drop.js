13bdcfd3f14e1054de79921fed58b42ea25be504
Larry Reid <lcreid@jadesystems.ca>
2017-05-31 22:08:41 -0700

    Semaphore approach seems to work.

--- app/assets/javascripts/drag-and-drop.js
+++ app/assets/javascripts/drag-and-drop.js
@@ -3,10 +3,11 @@ $(document).on('turbolinks:load', function() {
   // and modified
   var dragged;
   var saved_background = null;
+  var depth = 0;
 
   function set_background(element) {
     // console.log('Should I set background on element: ' + element.tagName);
-    if (saved_background === null) {
+    if (depth++ === 0) {
       saved_background = element.style.backgroundColor;
       element.style.backgroundColor = "#f0f0f0";
       // console.log("Yes. I saved: " + saved_background);
@@ -15,8 +16,10 @@ $(document).on('turbolinks:load', function() {
 
   function restore_background(element) {
     // console.log("I'm going to restore background on element: " + element.tagName + " to " + saved_background);
-    element.style.backgroundColor = saved_background;
-    saved_background = null;
+    if (--depth === 0){
+      element.style.backgroundColor = saved_background;
+      saved_background = null;
+    }
   }
 
   $(".js-draggable").attr("draggable", true);
@@ -25,6 +28,7 @@ $(document).on('turbolinks:load', function() {
     // console.log("dragstart: " + this.tagName + " " + this.className);
     dragged = event.target;
     event.target.style.opacity = 0.5;
+    depth = 0;
   });
 
   $(".js-droppable").on("drag", function(event) {
@@ -43,24 +47,24 @@ $(document).on('turbolinks:load', function() {
   });
 
   $(".js-droppable").on("dragenter", function(event) {
-    console.log("dsragenter this: " + this.tagName + " " + this.className);
-    console.log("dragenter target: " + event.target.tagName + " " + event.target.className);
+    // console.log("dsragenter this: " + this.tagName + " " + this.className);
+    // console.log("dragenter target: " + event.target.tagName + " " + event.target.className);
     // if (event.target === this) {
       set_background(this);
     // }
   });
 
   $(".js-droppable").on("dragleave", function(event) {
-    console.log("dragleave this: " + this.tagName + " " + this.className);
-    console.log("dragleave target: " + event.target.tagName + " " + event.target.className);
+    // console.log("dragleave this: " + this.tagName + " " + this.className);
+    // console.log("dragleave target: " + event.target.tagName + " " + event.target.className);
     // if (event.target === this) {
       restore_background(this);
     // }
   });
 
   $(".js-droppable").on("drop", function(event) {
-    console.log("drosp: " + this.tagName + " " + this.className);
-    console.log("drosp: " + event.target.tagName + " " + event.target.className);
+    // console.log("drop: " + this.tagName + " " + this.className);
+    // console.log("drop: " + event.target.tagName + " " + event.target.className);
     // console.log("preventing default");
     event.preventDefault();
     // if (event.target === this) {
